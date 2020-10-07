b6be1630693a10f42400cafc93c29c6febd4c8c8
Jaan Susi <jaan.susi@gmail.com>
2020-10-07 22:12:36 +0300

    Initialized loopback setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,39 @@
-var express = require("express");
-var app = express();
+import {ApplicationConfig, RahvaviisidapiApplication} from './application';
 
-const port = 3000;
+export * from './application';
 
-app.get('/', (req : any, res : any, next : any) => {
-    res.send('Server running');
-});
+export async function main(options: ApplicationConfig = {}) {
+  const app = new RahvaviisidapiApplication(options);
+  await app.boot();
+  await app.start();
 
-app.listen(port, () => {
-    console.log("Server running on port 3000");
-});
+  const url = app.restServer.url;
+  console.log(`Server is running at ${url}`);
+  console.log(`Try ${url}/ping`);
+
+  return app;
+}
+
+if (require.main === module) {
+  // Run the application
+  const config = {
+    rest: {
+      port: +(process.env.PORT ?? 3000),
+      host: process.env.HOST,
+      // The `gracePeriodForClose` provides a graceful close for http/https
+      // servers with keep-alive clients. The default value is `Infinity`
+      // (don't force-close). If you want to immediately destroy all sockets
+      // upon stop, set its value to `0`.
+      // See https://www.npmjs.com/package/stoppable
+      gracePeriodForClose: 5000, // 5 seconds
+      openApiSpec: {
+        // useful when used with OpenAPI-to-GraphQL to locate your application
+        setServersFromRequest: true,
+      },
+    },
+  };
+  main(config).catch(err => {
+    console.error('Cannot start the application.', err);
+    process.exit(1);
+  });
+}
