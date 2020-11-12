9646a4290d81068c9560a5cf606aded0869f79ee
Jaan Susi <jaan.susi@gmail.com>
2020-11-12 23:58:20 +0200

    Lisasin menüüle tõlke

--- frontend/src/Header/Header.js
+++ frontend/src/Header/Header.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import { useTranslation } from "react-i18next";
 import './Header.css';
 
 const Header = (() => {
+    const { t, i18n } = useTranslation('common');
     return (
         <div id="header-container">
-            <div className="header-link">
-                <Link to="/">Home</Link>
+            <div className="language-container">
+                <button onClick={() => i18n.changeLanguage('et')}>est</button>
+                <button onClick={() => i18n.changeLanguage('en')}>eng</button>
             </div>
             <div className="header-link">
-                <Link to="/persons">Isikud</Link>
+                <Link to="/">{t('common.home')}</Link>
             </div>
             <div className="header-link">
-                <Link to="/tunes">Viisid</Link>
+                <Link to="/persons">{t('common.persons')}</Link>
+            </div>
+            <div className="header-link">
+                <Link to="/tunes">{t('common.tunes')}</Link>
             </div>
         </div>
     );
