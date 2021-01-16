781aad9e9dadbed39191ccd6d27f59c0c7e3ac99
Jaan Susi <jaan.susi@gmail.com>
2021-01-16 16:39:57 +0200

    Menüü korda

--- frontend/src/Header/Header.js
+++ frontend/src/Header/Header.js
@@ -3,33 +3,55 @@ import { Link } from 'react-router-dom';
 import { useTranslation } from "react-i18next";
 import './Header.css';
 import logo from '../assets/logo.png';
+import { Grid, Button, MenuList, Menu, MenuItem } from '@material-ui/core';
 
 const Header = (() => {
     const { t, i18n } = useTranslation('common');
-    let [langOptionsVisible, setLangOptionsVisible] = useState(false);
-    const ToggleLanguages = (() => {
-        setLangOptionsVisible(!langOptionsVisible);
-        console.log(langOptionsVisible);
-    });
+
+    const [anchorEl, setAnchorEl] = useState(null);
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = (event) => {
+        setAnchorEl(null);
+        switch (event.target.textContent) {
+            case t('language.est'):
+                i18n.changeLanguage('et');
+                break;
+            case t('language.eng'):
+                i18n.changeLanguage('en');
+                break;
+            default:
+                i18n.changeLanguage('et');
+                break;
+        }
+    };
+
     return (
-        <div id="header-container">
-            <div id="logo-container">
+        <Grid id="header-container" container direction='row'>
+            <Grid xs={5} id="logo-container">
                 <a href="/"><img src={logo} alt="Logo"></img></a>
-            </div>
-            <div id="link-container">
-                <Link className="header-link" to="/">{t('common.home')}</Link>
-                <Link className="header-link" to="/isikud">{t('common.persons')}</Link>
-                <Link className="header-link" to="/viisid">{t('common.tunes')}</Link>
-                <Link className="header-link" to="/klassifikaatorid">{t('common.classificators')}</Link>
-            </div>
-            <div id="language-container">
-                <button className="select-language" onClick={ToggleLanguages}>{t('language.current')}</button>
-                <div id="language-choices" className={langOptionsVisible ? "visible" : "hidden"}>
-                    <button className="language-button" onClick={() => i18n.changeLanguage('et')}>{t('language.est')}</button>
-                    <button className="language-button" onClick={() => i18n.changeLanguage('en')}>{t('language.eng')}</button>
-                </div>
-            </div>
-        </div>
+            </Grid>
+            <Grid xs={5} direction='row' container justify='center' alignItems='center'>
+                <MenuList className='menu'>
+                    <MenuItem component={Link} to='/'>{t('common.home')}</MenuItem>
+                    <MenuItem component={Link} to='/isikud'>{t('common.persons')}</MenuItem>
+                    <MenuItem component={Link} to='/viisid'>{t('common.tunes')}</MenuItem>
+                    <MenuItem component={Link} to='/klassifikaatorid'>{t('common.classificators')}</MenuItem>
+                </MenuList>
+            </Grid>
+            <Grid xs={2} direction='row' container justify='center' alignItems="center" id="language-container">
+                <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+                    {t('language.current')}
+                </Button>
+                <Menu anchorEl={anchorEl} keepMounted open={Boolean(anchorEl)} onClose={handleClose}>
+                    <MenuItem onClick={handleClose}>{t('language.est')}</MenuItem>
+                    <MenuItem onClick={handleClose}>{t('language.eng')}</MenuItem>
+                </Menu>
+            </Grid>
+        </Grid>
     );
 });
 
