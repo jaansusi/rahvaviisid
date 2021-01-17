c1630903a20cb4baa862a2b54a24a104c6c3cf95
Jaan Susi <jaan.susi@gmail.com>
2021-01-17 11:40:08 +0200

    Puuduvad klassifikaatorid ka lisatud

--- frontend/src/Classificators/ClassificatorTypes/ClassificatorTypeMap.js
+++ frontend/src/Classificators/ClassificatorTypes/ClassificatorTypeMap.js
@@ -44,118 +44,180 @@ const ClassificatorsMap =
                     name: 'tune.language',
                     url: 'keel',
                     apiPath: 'languages'
+                },
+                {
+                    name: 'tune.country',
+                    url: 'riik',
+                    apiPath: 'countries'
+                },
+                {
+                    name: 'tune.state',
+                    url: 'viisi-seisund',
+                    apiPath: 'tune-states'
+                },
+                {
+                    name: 'tune.genre',
+                    url: 'viisi-liik',
+                    apiPath: 'tune-genres'
+                }
+            ]
+        },
+        {
+            name: 'person.person',
+            classificators: [
+                {
+                    name: 'person.role',
+                    url: 'viisi-tegija-roll',
+                    apiPath: 'tune-person-role-types'
+                },
+                {
+                    name: 'user.role',
+                    url: 'kasutaja-roll',
+                    apiPath: 'user-role-types'
+                },
+                {
+                    name: 'person.sex',
+                    url: 'sugu',
+                    apiPath: 'sexes'
+                }
+            ]
+        },
+        {
+            name: 'place.place',
+            classificators: [
+                {
+                    name: 'place.type',
+                    url: 'koha-liik',
+                    apiPath: 'tune-place-types'
+                },
+                {
+                    name: 'place.parish',
+                    url: 'kihelkond',
+                    apiPath: 'parishes'
+                },
+                {
+                    name: 'place.municipality',
+                    url: 'vald',
+                    apiPath: 'municipalities'
+                },
+                {
+                    name: 'place.village',
+                    url: 'küla',
+                    apiPath: 'villages'
+                }
+            ]
+        },
+        {
+            name: 'performance.performance',
+            classificators: [
+                {
+                    name: 'performance.actual.type',
+                    url: 'tegeliku-esituse-liik',
+                    apiPath: 'actual-performance-types'
+                },
+                {
+                    name: 'performance.traditional.type',
+                    url: 'traditsioonilise-esituse-liik',
+                    apiPath: 'traditional-performance-types'
+                },
+                {
+                    name: 'performance.actual.action',
+                    url: 'tegeliku-tegevuse-liik',
+                    apiPath: 'actual-action-types'
+                },
+                {
+                    name: 'performance.traditional.action',
+                    url: 'traditsioonilise-tegevuse-liik',
+                    apiPath: 'traditional-action-types'
+                }
+            ]
+        },
+        {
+            name: 'song.song',
+            classificators: [
+                {
+                    name: 'song.genre',
+                    url: 'laulu-liik',
+                    apiPath: 'song-genres'
+                },
+                {
+                    name: 'song.topic',
+                    url: 'laulu-teema',
+                    apiPath: 'song-topics'
+                },
+                {
+                    name: 'song.verse',
+                    url: 'varsivorm',
+                    apiPath: 'verse-forms'
+                }
+            ]
+        },
+        {
+            name: 'song.song',
+            classificators: [
+                {
+                    name: 'attribute.textForm',
+                    url: 'teksti-vorm',
+                    apiPath: 'text-forms'
+                },
+                {
+                    name: 'attribute.tuneForm',
+                    url: 'viisi-vorm',
+                    apiPath: 'tune-forms'
+                },
+                {
+                    name: 'attribute.soundRange',
+                    url: 'heliulatus',
+                    apiPath: 'sound-ranges'
+                },
+                {
+                    name: 'attribute.rhythmType',
+                    url: 'rytmityyp',
+                    apiPath: 'rhythm-types'
+                }
+            ]
+        },
+        {
+            name: 'transcription.transcription',
+            classificators: [
+                {
+                    name: 'transcription.source',
+                    url: 'noodistuse-alus',
+                    apiPath: 'transcription-sources'
+                },
+                {
+                    name: 'transcription.personRole',
+                    url: 'noodistuse-tegija-roll',
+                    apiPath: 'transcription-person-role-types'
+                }
+            ]
+        },
+        {
+            name: 'coding.coding',
+            classificators: [
+                {
+                    name: 'coding.keySignature',
+                    url: 'votmemark',
+                    apiPath: 'key-signatures'
+                },
+                {
+                    name: 'coding.supportSound',
+                    url: 'tugiheli',
+                    apiPath: 'support-sounds'
+                },
+                {
+                    name: 'coding.pitch',
+                    url: 'korgus',
+                    apiPath: 'pitches'
+                },
+                {
+                    name: 'coding.measure',
+                    url: 'taktimoot',
+                    apiPath: 'measures'
                 }
             ]
         }
     ]
 };
-//         'keel': {
-//             'apiPath': 'languages'
-//         },
-//         'riik': {
-//             'apiPath': 'countries'
-//         },
-//         'viisi-seisund': {
-//             'apiPath': 'tune-states'
-//         },
-//         'viisi-liik': {
-//             'apiPath': 'tune-genres'
-//         }
-//     },
-
-
-//     // Person classificators
-
-//     'viisi-tegija-roll': {
-//         'apiPath': 'tune-person-role-types'
-//     },
-//     'kasutaja-roll': {
-//         'apiPath': 'user-role-types'
-//     },
-//     'sugu': {
-//         'apiPath': 'sexes'
-//     },
-
-//     // Place classificators
-
-//     'koha-liik': {
-//         'apiPath': 'tune-place-types'
-//     },
-//     'kihelkond': {
-//         'apiPath': 'parishes'
-//     },
-//     'vald': {
-//         'apiPath': 'municipalities'
-//     },
-//     'kyla': {
-//         'apiPath': 'villages'
-//     },
-
-//     // Performance classificators
-
-//     'tegeliku-esituse-liik': {
-//         'apiPath': 'actual-performance-types'
-//     },
-//     'traditsioonilise-esituse-liik': {
-//         'apiPath': 'traditional-performance-types'
-//     },
-//     'tegeliku-tegevuse-liik': {
-//         'apiPath': 'actual-action-types'
-//     },
-//     'traditsioonilise-tegevuse-liik': {
-//         'apiPath': 'traditional-action-types'
-//     },
-
-//     // Song classificators
-
-//     'laulu-liik': {
-//         'apiPath': 'song-genres'
-//     },
-//     'laulu-teema': {
-//         'apiPath': 'song-topics'
-//     },
-//     'varsivorm': {
-//         'apiPath': 'verse-forms'
-//     },
-
-//     // Musical classificators
-
-//     'teksti-vorm': {
-//         'apiPath': 'text-forms'
-//     },
-//     'viisi-vorm': {
-//         'apiPath': 'tune-forms'
-//     },
-//     'heliulatus': {
-//         'apiPath': 'sound-ranges'
-//     },
-//     'rytmityyp': {
-//         'apiPath': 'rhythm-types'
-//     },
-
-//     // Transcription classificators
-
-//     'noodistuse-alus': {
-//         'apiPath': 'transcription-sources'
-//     },
-//     'noodistuse-tegija-roll': {
-//         'apiPath': 'transcription-person-role-types'
-//     },
-
-//     // Encoding classificators
-
-//     'votmemark': {
-//         'apiPath': 'key-signatures'
-//     },
-//     'tugiheli': {
-//         'apiPath': 'support-sounds'
-//     },
-//     'korgus': {
-//         'apiPath': 'pitches'
-//     },
-//     'taktimoot': {
-//         'apiPath': 'measures'
-//     },
-// };
 
 export default ClassificatorsMap;
--- frontend/src/Classificators/Classificators.js
+++ frontend/src/Classificators/Classificators.js
@@ -28,44 +28,6 @@ const Classificators = (() => {
                 })
             }
         </Grid>
-        // <h3>{t('tune.tune')}</h3>
-        // <Button className='action-link' href={`${path}/rahvas`}>{t('tune.nation')}</Button>
-        // <Button className='action-link' href={`${path}/keel`}>{t('tune.language')}</Button>
-        // <Button className='action-link' href={`${path}/riik`}>{t('tune.country')}</Button>
-        // <Button className='action-link' href={`${path}/viisi-seisund`}>{t('tune.state')}</Button>
-        // <Button className='action-link' href={`${path}/viisi-liik`}>{t('tune.genre')}</Button>
-        // <h3>{t('person.person')}</h3>
-        // <Button className='action-link' href={`${path}/viisi-tegija-roll`}>{t('person.role')}</Button>
-        // <Button className='action-link' href={`${path}/kasutaja-roll`}>{t('user.role')}</Button>
-        // <Button className='action-link' href={`${path}/sugu`}>{t('person.sex')}</Button>
-        // <h3>{t('place.place')}</h3>
-        // <Button className='action-link' href={`${path}/koha-liik`}>{t('place.type')}</Button>
-        // <Button className='action-link' href={`${path}/kihelkond`}>{t('place.parish')}</Button>
-        // <Button className='action-link' href={`${path}/vald`}>{t('place.municipality')}</Button>
-        // <Button className='action-link' href={`${path}/kyla`}>{t('place.village')}</Button>
-        // <h3>{t('performance.performance')}</h3>
-        // <Button className='action-link' href={`${path}/tegeliku-esituse-liik`}>{t('performance.actual.type')}</Button>
-        // <Button className='action-link' href={`${path}/traditsioonilise-esituse-liik`}>{t('performance.traditional.type')}</Button>
-        // <Button className='action-link' href={`${path}/tegeliku-tegevuse-liik`}>{t('performance.actual.action')}</Button>
-        // <Button className='action-link' href={`${path}/traditsioonilise-tegevuse-liik`}>{t('performance.traditional.action')}</Button>
-        // <h3>{t('song.song')}</h3>
-        // <Button className='action-link' href={`${path}/laulu-liik`}>{t('song.genre')}</Button>
-        // <Button className='action-link' href={`${path}/laulu-teema`}>{t('song.topic')}</Button>
-        // <Button className='action-link' href={`${path}/varsivorm`}>{t('song.verse')}</Button>
-        // <h3>{t('attribute.attribute')}</h3>
-        // <Button className='action-link' href={`${path}/teksti-vorm`}>{t('attribute.textForm')}</Button>
-        // <Button className='action-link' href={`${path}/viisi-vorm`}>{t('attribute.tuneForm')}</Button>
-        // <Button className='action-link' href={`${path}/heliulatus`}>{t('attribute.soundRange')}</Button>
-        // <Button className='action-link' href={`${path}/rytmityyp`}>{t('attribute.rhythmType')}</Button>
-        // <h3>{t('transcription.transcription')}</h3>
-        // <Button className='action-link' href={`${path}/noodistuse-alus`}>{t('transcription.source')}</Button>
-        // <Button className='action-link' href={`${path}/noodistuse-tegija-roll`}>{t('transcription.personRole')}</Button>
-        // <h3>{t('coding.coding')}</h3>
-        // <Button className='action-link' href={`${path}/votmemark`}>{t('coding.keySignature')}</Button>
-        // <Button className='action-link' href={`${path}/tugiheli`}>{t('coding.supportSound')}</Button>
-        // <Button className='action-link' href={`${path}/korgus`}>{t('coding.pitch')}</Button>
-        // <Button className='action-link' href={`${path}/taktimoot`}>{t('coding.measure')}</Button>
-        // </>
     );
 });
 
