7a59e49f4429c8ee49fed17615659fdb5d8d57df
Jaan Susi <jaan.susi@gmail.com>
2021-01-21 00:55:20 +0200

    Hunnik Dockeri asju

--- backend/src/datasources/db.datasource.ts
+++ backend/src/datasources/db.datasource.ts
@@ -4,12 +4,11 @@ import {juggler} from '@loopback/repository';
 const config = {
   name: 'db',
   connector: 'postgresql',
-  url: '',
-  host: 'localhost',
-  port: 5432,
-  user: 'local_dev_username',
-  password: 'local_dev_password',
-  database: 'kivi'
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
 };
 
 // Observe application's life cycle to disconnect the datasource when
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
 let config = {
-    apiUrl: 'http://localhost:3000'//'http://viisidapi.xn--x-dha.ee'
+    apiUrl: 'viisidapi.xn--x-dha.ee'
 }
 
 export default config;
