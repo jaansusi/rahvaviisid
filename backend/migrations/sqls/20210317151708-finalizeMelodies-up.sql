c4a84c4fd4708a10ff36d6e76f432d6619648be7
Jaan Susi <jaan.susi@gmail.com>
2021-03-17 18:12:10 +0200

    Et midagi kaduma ei läheks

---
+++ backend/migrations/sqls/20210317151708-finalizeMelodies-up.sql
@@ -0,0 +1,34 @@
+DROP TABLE folk_tune.tune_variations;
+
+ALTER TABLE folk_tune.tune_melodies
+    ADD COLUMN variation_index integer;
+
+ALTER TABLE folk_tune.tune_melodies DROP COLUMN clef;
+
+ALTER TABLE folk_tune.tune_melodies
+    ADD COLUMN rhythm_type_id integer;
+
+ALTER TABLE folk_tune.tune_melodies
+    ADD CONSTRAINT fk_tune_melodies_rhythm_types FOREIGN KEY (rhythm_type_id)
+    REFERENCES folk_tune.rhythm_types (id)
+    ON UPDATE NO ACTION
+    ON DELETE NO ACTION
+    NOT VALID;
+
+ALTER TABLE folk_tune.tunes DROP COLUMN variation;
+
+ALTER TABLE folk_tune.tunes
+    ADD COLUMN clef text;
+
+ALTER TABLE folk_tune.tunes
+    ADD COLUMN support_sound text;
+
+ALTER TABLE folk_tune.tunes
+    ADD COLUMN height text;
+
+ALTER TABLE folk_tune.tunes
+    ADD COLUMN bar text;
+
+ALTER TABLE folk_tune.tune_melodies
+    RENAME rhythm_type_id TO rhythm_type;
+ALTER TABLE folk_tune.tune_melodies DROP CONSTRAINT fk_tune_melodies_rhythm_types;
