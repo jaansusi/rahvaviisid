b82938f1aaee14c48fb652a99e9efc413ddbb487
Jaan Susi <jaan.susi@gmail.com>
2021-05-26 22:00:08 +0300

    migratsiooni test

--- backend/createConfig.sh
+++ backend/createConfig.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo '{"createDB":{"driver":"pg","host":"$DB_HOST","port":"$DB_PORT","user":"$DB_USERNAME","password":"$DB_PASSWORD"},"migrate":"driver": "pg","host": "$DB_HOST","port": "$DB_PORT","user": "$DB_USERNAME","password": "$DB_PASSWORD","database": "$DB_DATABASE"}}' > migrations/database.json
+echo \{\"createDB\":\{\"driver\":\"pg\",\"host\":\"$DB_HOST\",\"port\":\"$DB_PORT\",\"user\":\"$DB_USERNAME\",\"password\":\"$DB_PASSWORD\"\},\"migrate\":\"driver\": \"pg\",\"host\": \"$DB_HOST\",\"port\": \"$DB_PORT\",\"user\": \"$DB_USERNAME\",\"password\": \"$DB_PASSWORD\",\"database\": \"$DB_DATABASE\"\} > migrations/database.json
