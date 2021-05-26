d779946269726537e790d5ec14d99447cc85d860
Jaan Susi <jaan.susi@gmail.com>
2021-05-26 22:26:47 +0300

    GG

--- backend/createConfig.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-echo \{\"createDB\":\{\"driver\":\"pg\",\"host\":\"$DB_HOST\",\"port\":\"$DB_PORT\",\"user\":\"$DB_USERNAME\",\"password\":\"$DB_PASSWORD\"\},\"migrate\":\{\"driver\": \"pg\",\"host\": \"$DB_HOST\",\"port\": \"$DB_PORT\",\"user\": \"$DB_USERNAME\",\"password\": \"$DB_PASSWORD\",\"database\": \"$DB_DATABASE\"\}\} > migrations/database.json
