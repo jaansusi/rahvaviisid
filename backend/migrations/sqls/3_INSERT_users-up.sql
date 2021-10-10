52fb79c945337eb73cae2341563042009f6bf00e
martin.rajur <martin.rajur@omniva.ee>
2021-10-10 14:10:01 +0300

    new sql from Kristi, broken commit

---
+++ backend/migrations/sqls/3_INSERT_users-up.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (person_id,user_role_type_id,username,"password",is_active) VALUES
+	 (637,1,'taivelohmuse@netscape.net','$2y$10$4EuLXgOaL5O08OFPjb2.cedd3hOjMJcyOiXM7vm.KiZKKg..76rBi',true),
+	 (1718,3,'helen','$2y$10$BpEAqZucmoQRfqXLBEHSZOrZHaaz23jvgybJT4AZ.oz8ngSK4a1nC',false);
